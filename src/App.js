cc14c62737c673a9486048f4cd26206e63e1beb9
sara <saraslemanalhamede@gmail.com>
2025-04-21 10:53:18 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. saved
         </p>
+        <h1>hi sara</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
